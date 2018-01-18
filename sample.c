0fda1de13544c391010c96efabfd19b955ef39ec
ARVIND SAI K <arvindsaik@users.noreply.github.com>
2018-01-19 00:20:05 +0530

    Update sample.c

--- sample.c
+++ sample.c
@@ -3,12 +3,10 @@
 /* 1 ) Test for identifying int and char data types and their corresponding sub-types
 like short , long , signed, unsigned.
 2 ) Test for identifying while and nested while constructs  */
-
-int comp(int a)
+int compu(int a)
 {
-
+    
 }
-
 int main(){
     /* test for various integer types supported */
     short int var1;
@@ -17,18 +15,18 @@ int main(){
     int var4;
     signed short int var5;
     signed long int var6;
-    signed long int var7;
+    signed long long int var7;
     signed int var8;
-    short int var5;
+    unsigned short int var5;
     unsigned long int var6;
     unsigned long long int var7;
     unsigned int var8;
 
     /* test for various character types supported */
-    char var9;
+    char var9 = 'b';
     signed char var10;
     signed char var11;
-
+    float var12 = 9.56;
     /* test for while and nested while */
     var1 = 0;
     while(var1 < 20){
@@ -45,6 +43,6 @@ int main(){
         var2 = 0;
         var1 = var1 + 1;
     }
-
+    printf("\nDone\n");
     return 0;
 }
