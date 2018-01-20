0690a6593be39d966e4968ce6e47340dbafbaaa8
derikclive <derikclive@gmail.com>
2018-01-20 21:05:05 +0530

    Formatting done

--- test-suit/test1.c
+++ test-suit/test1.c
@@ -3,13 +3,17 @@
 /* 1 ) Test for identifying int and char data types and their corresponding sub-types
 like short , long , signed, unsigned.
 2 ) Test for identifying while and nested while constructs  */
-
+int compu(int a)
+{
+    
+}
 int main(){
     /* test for various integer types supported */
     short int var1;
     long int var2;
     long long int var3;
     int var4;
+    int $cd;
     signed short int var5;
     signed long int var6;
     signed long long int var7;
@@ -18,13 +22,13 @@ int main(){
     unsigned long int var6;
     unsigned long long int var7;
     unsigned int var8;
-    
+
     /* test for various character types supported */
-    char var9;
+    char var9 != 'b';
     signed char var10;
     signed char var11;
-
-    /* test for while and nested while */ 
+    float var12 = 9.56;
+    /* test for while and nested while */
     var1 = 0;
     while(var1 < 20){
         var2 = 0;
@@ -40,6 +44,6 @@ int main(){
         var2 = 0;
         var1 = var1 + 1;
     }
-
+    printf("\nDone\n");
     return 0;
 }
