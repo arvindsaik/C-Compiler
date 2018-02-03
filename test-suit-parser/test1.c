991d726c09d5be1f1aa023df41bcb51c994528ef
derikclive <derikclive@gmail.com>
2018-02-03 22:55:33 +0530

    New test case

---
+++ test-suit-parser/test1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* No errors just testing if-else, if-else ladder */
+
+int main(){
+   int a = 1;
+   int b = 2;
+   int c = 3;
+   int max;
+   if(a >= b){
+       if(a >= c){
+           max = a;
+       }
+       else{
+           max = c;
+       }
+   }
+   else{
+       if(b >= c){
+           max = b;
+       }
+       else{
+           max = c;
+       }
+   }
+
+   if(max > 2){
+       printf("Good! \n");
+   }
+
+   return 0;
+}
